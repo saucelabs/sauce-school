475ef1d11ab37fce78a21bf02e99f90c9216665e
Félix P <felix@saucelabs.com>
2020-12-16 10:17:14 -0800

    Fix title test

--- cypress/integration/title.js
+++ cypress/integration/title.js
@@ -2,6 +2,6 @@ describe(`Testing ${Cypress.env('HOST_URL')}`, () => {
 	it('.title() - Checking page title', () => {
 		cy.log(`Url: ${Cypress.env('HOST_URL')}`)
 		cy.visit(`${Cypress.env('HOST_URL')}`)
-		cy.title().should('eq', 'Sauce School Training')
+		cy.title().should('eq', 'Sauce School Training | Sauce Labs')
 	})
 })
