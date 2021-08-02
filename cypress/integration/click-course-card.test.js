60f94ba246948df77d481c50efa3abc6927ed36d
Lindsay Walker <lindsay.waklker@saucelabs.com>
2021-08-02 14:54:57 -0500

    changed click to Start

--- cypress/integration/click-course-card.test.js
+++ cypress/integration/click-course-card.test.js
@@ -2,7 +2,7 @@ describe('Click on Selenium Java Course', function () {
   it('Visits Sauce School Landing Checks for Course Cards', function (){
     cy.visit(`${Cypress.env('HOST_URL')}`)
 
-    cy.contains('Selenium Java').click()
+    cy.contains('Start').click()
 
     cy.url()
       .should('include', '/SeleniumJava')
